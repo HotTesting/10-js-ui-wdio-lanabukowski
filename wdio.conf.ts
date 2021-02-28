f45f9995ccaf867c8e98a9e2ae34494fc798144f
Lana <59263160+lanabukowski@users.noreply.github.com>
2021-02-28 11:26:38 +0200

    Lesson 2

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -90,7 +90,7 @@ export const config: WebdriverIO.Config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'http://ip-6147.proline.net.ua:10082/',
+    baseUrl: 'https://demo.opencart.com/index.php',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
